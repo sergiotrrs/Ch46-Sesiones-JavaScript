4ac44c14f4bdca10a045c81d83980a0c82fa6bdd
Sergio Torres <sergio.torres@idr.edu.mx>
2024-09-26 11:56:21 -0600

    feat: archivos html, css, js para js02 funciones

---
+++ js02-funciones/script.js
@@ -0,0 +1,49 @@
+console.log("Sesión JS 02 - funciones");
+
+
+/*
+https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Functions
+
+Función: Conjunto de instrucciones que realiza una tarea o calcula un valor.
+  Código que se puede reutilizar.
+
+
+  ----------- Funciones declaradas --------------
+    (function declaration, function statement)
+Una característica de las funciones declaradas es que tien hoisting.
+
+sintaxis:
+    function nombreFuncionCamelCase ( parámetros ){
+        //cuerpo de la función
+        instrucciones;
+    }
+
+hoisting: comportamiento en JS que permite a las declaración de variables(var)
+o funciones se eleven al comienzo de su ámbito antes de que se ejecute el código.
+
+*/
+
+console.log( greeting()   );
+
+function greeting(){
+    return "Hola Ch46";
+}
+
+/*
+ ------------ Funciones expresadas -----------------------
+            (function expressions)
+Son funciones declaradas dentro de la asignación de una variable.
+Estas funciones pueden ser anónimas ( no tienen nombre ).
+Las funciones expresadas no tiene hoisting, porque no se
+carga en memoria hasta que se utilice.
+
+sintaxis:
+    const nombreVariable = function nombreFuncion (parámetros){
+        instrucciones;
+    };
+*/
+
+const sum = function sumatoria( a, b) {
+    return a + b;
+}
+
