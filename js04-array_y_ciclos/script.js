639e37f7ee8594e4ca9ee3f4065a2779b7d6a92a
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-02 15:32:38 -0600

    feat: agregar header y footer a las cards

--- js04-array_y_ciclos/script.js
+++ js04-array_y_ciclos/script.js
@@ -358,14 +358,19 @@ console.table(productosGecko);
 // const listProductosGecko = productosGecko.map((producto) => `<li>${producto}</li>`);
 const listProductosGecko = productosGecko.map((producto) => `
   <div class="col-6 col-md-4 mt-4">
-    <div class="card" >
-      <div class="card-body">
+    <div class="card" style="min-height:19rem">
+      <div class="card-header" style="min-height:6rem">
         <h5 class="card-title">${producto}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>      
+      </div>         
+      <div class="card-body">         
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>        
+      </div>
+      <div class="card-footer">  
         <a href="#" class="card-link">Card link</a>
         <a href="#" class="card-link">Another link</a>
-      </div>
+      </div>         
+
     </div>
   </div>
 
