979958b094d6bbfff9d36e1dc097329b751c4c3c
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-03 11:02:34 -0600

    feat: verificar si 3 numeros tienen el ultimo digito igual

--- js05-operadores/script.js
+++ js05-operadores/script.js
@@ -10,16 +10,68 @@ const categorias = [
   "Incremento y decremento",
   "Ternarios"
 ];
-console.table( categorias );
-
 
 const listItems = categorias.map( (categoria)=> `<li> ${categoria} </li>` );
-console.table( listItems );
-
 const refCategorias = document.getElementById("categorias");
-console.log(refCategorias.innerHTML);
-
 const stringListItems = listItems.join("");
-console.log( stringListItems );
-
 refCategorias.innerHTML = stringListItems;
+
+//===============================================
+// ---------------- Opéradores aritméticos ----------------
+/* 
+   adición +
+   multiplicación *
+   substracción -
+   división /
+   resto %
+   exponencial **  - Math.pow(2, 3)   2**3
+*/
+// Se realiza una conversión implícita
+console.log("3" *  "3"); // 9
+console.log("5" - 4 ); // 1
+console.log("10" / 2 ); // 5
+console.log( "20" % 4 ); // 0
+// Saber si 3 números terminan con el mis dígito
+// Ejemplo 1098, 78,  8  -> true
+//           77,  6, 23  -> false
+
+const verificarUltimoDigitoUsandoString = (numA, numB, numC) =>{
+  const strNumA = String( numA );
+  const strNumB = String( numB );
+  const strNumC = String( numC );
+
+  const lastDigitNumA = strNumA[ strNumA.length - 1 ];
+  const lastDigitNumB = strNumB[ strNumB.length - 1 ];
+  const lastDigitNumC = strNumC[ strNumC.length - 1 ];
+
+  const result =  lastDigitNumA === lastDigitNumB && lastDigitNumA === lastDigitNumC;
+  return result;
+}
+
+const verificarUltimoDigitoUsandoResiduo = (numA, numB, numC) =>{
+  const lastDigitNumA = numA % 10; 
+  const lastDigitNumB = numB % 10; 
+  const lastDigitNumC = numC % 10; 
+  
+  const result = lastDigitNumA === lastDigitNumB && lastDigitNumA === lastDigitNumC;
+  return result;
+}
+
+
+console.log(verificarUltimoDigitoUsandoString( 1098, 78, 8 ) ); // true
+console.log(verificarUltimoDigitoUsandoString( 77, 6, 23 ) ); // false
+
+console.log(verificarUltimoDigitoUsandoResiduo( 1098, 78, 8 ) ); // true
+console.log(verificarUltimoDigitoUsandoResiduo( 77, 6, 23 ) ); // false
+
+
+
+
+// Uso de rest parameters ( ...restParameters )
+const verificarUltimoDigitoDeVariosNumeros = ( name, ...numeros  ) =>{
+  console.log( name, numeros );
+
+}
+console.log( verificarUltimoDigitoDeVariosNumeros("Eli", 2, 22 ) ); // true
+console.log( verificarUltimoDigitoDeVariosNumeros("Eli", 2, 22, 52 ) ); // true
+console.log( verificarUltimoDigitoDeVariosNumeros("Eli", 2, 22, 52, 44 ) ); // false
