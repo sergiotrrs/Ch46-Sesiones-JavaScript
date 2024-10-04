996a82c37b1b0dafa49255bf9bcb836c63235335
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-04 12:42:30 -0600

    feat: metodo changeElementById()

--- js06-dom/script.js
+++ js06-dom/script.js
@@ -1,1 +1,19 @@
 console.log("Sesión JS06 - DOM");
+
+/**
+ *  Cambiar el contenido de un elemento HTML por medio de su ID
+ */
+const changeElementById = (idName, newMessage) => {
+  const refElement = document.getElementById(idName);
+  // const refElement = document.getElementById("title");
+  console.log(refElement);
+
+  // refElement.textContent = 
+  refElement.innerHTML  = newMessage;
+
+};
+
+changeElementById("title", "Es tan fetch la sesión");
+
+// Ejercicio: Modifica el texto del h2 a "Get in loser we're going shopping!"
+
