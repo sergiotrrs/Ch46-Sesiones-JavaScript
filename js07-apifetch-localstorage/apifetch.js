a5e6f15c051848429c23d41f33833df01c1b34f1
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-07 13:19:43 -0600

    feat: api fetch

---
+++ js07-apifetch-localstorage/apifetch.js
@@ -0,0 +1,37 @@
+console.log("JS07- API FETCH");
+
+/*
+ Las promesa es un patrón asincrónico que se utiliza
+ para manejar operaciones asíncronas.
+
+ Las promesas permite realizar tareas asíncronas y
+ manejar los resultados (éxito o error) en un momento posterior.
+
+ Las promesas tiene 3 estados:
+
+ 1.- Pending: El estado inicial de una promesa antes de que se resuelva o rechace
+ 2.- Resolved: La promesa se resuelve con un valor
+ 3.- Rejected: La promesa se rechaza con un razón
+
+Resolve y reject son funciones de callback.
+Para crear una promesa se usa el constructor de la clase Promise. 
+
+Para manejar una promesa se usa los métodos:
+ 1.- then() : Se ejecuta cuando la promesa se resuelve correctamente.
+ 2.- catch() :  Se ejecuta cuando la promesa es rechazada (error).
+ 3.- finally() : Se ejecuta al finalizar, independientemente del resultado.
+
+ Las promesas se utilizan mucho en operaciones como solicitudes HTTP con fetch, 
+ donde no se sabe cuándo se recibirán los datos del servidor.
+
+ La API Fetch nos ayuda a acceder y manipular 
+ peticiones HTTP ( GET, POST, PUT, DELETE).
+ 
+ Sintaxis:
+    fetch(resource, options)
+
+*/
+
+
+
+
