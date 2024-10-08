13bcebb47fa06f7973cc368d217b70e509c4f94c
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-08 12:48:08 -0600

    feat: implementacion de postUser()

--- js07-apifetch-localstorage/form-handle.js
+++ js07-apifetch-localstorage/form-handle.js
@@ -15,6 +15,39 @@ const register = document.forms["register-form"];
  * cambios de teclado, movimientos del mouse, entre otros.
  */
 
+const isValid = ( newUser ) =>{
+    let valid = true;
+
+    if( newUser.password.length < 8  || newUser.password.length > 51  ) {
+       alert("La longitud del password debe ser mayor a 8");
+       valid = false;
+    } else if( !newUser.checkbox ) {
+        alert("Debes aceptar los términos y condiciones");
+        valid = false;
+    }
+
+    return valid;
+}
+
+const postUser = async ( newUser ) => {
+   const url = "http://awc-demo.us-east-1.elasticbeanstalk.com/api/v1/users";
+
+   const options = { 
+    method: "POST", // POST, PUT, DELETE, GET
+    headers: { "Content-Type":"application/json" },
+    body:  JSON.stringify( newUser ) // conversión de Object a JSON
+   }
+
+   try {
+     const response = await fetch( url, options  );
+     const registeredUser = await response.json(); 
+     console.table( registeredUser );
+     alert("Felicidades, el registro ha sido exitoso");
+   } catch (error) {
+     console.warn(error);
+   }
+}
+
 // register.addEventListener( "submit" , ()=> {} );
 register.addEventListener( "submit" , ( event )=> {
     event.preventDefault();
@@ -31,5 +64,10 @@ register.addEventListener( "submit" , ( event )=> {
     }
 
     console.table( newUser );
+
+    // Verificar datos del formulario
+    if( isValid(newUser) === true ){
+        postUser( newUser); // enviar datos al servidor
+    } 
 
 } );
