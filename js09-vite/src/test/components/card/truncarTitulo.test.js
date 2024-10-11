474e8cf1404bc925d298bc275df76637eefcff3b
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-11 12:25:38 -0600

    test: no truncar los titulos

--- js09-vite/src/test/components/card/truncarTitulo.test.js
+++ js09-vite/src/test/components/card/truncarTitulo.test.js
@@ -1,6 +1,12 @@
 import { truncarTitulo } from "../../../components/card/truncarTitulo.js";
 
 // test( "Descripción", ()=>{} );
+
 test("Truncar los el título a 10 caracteres", () => {
-  expect(truncarTitulo("Born to Make You Happy", 10)).toBe("Born to...");
+  expect( truncarTitulo("Born to Make You Happy", 10)).toBe("Born to...");
+  expect( truncarTitulo("Estamos en la semana 7", 10 )).toBe("Estamos...");
+});
+
+test("No truncar los titulo menores a 20 caracteres", ()=>{
+    expect( truncarTitulo("Descripcion", 20) ).toBe("Descripcion");
 });
