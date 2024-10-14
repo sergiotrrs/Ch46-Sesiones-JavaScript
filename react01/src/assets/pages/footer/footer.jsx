12f786d6f537f5881cec7791d645616fc08c7ff6
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-14 13:30:14 -0600

    style: estilo en el componente Footer

--- react01/src/assets/pages/footer/footer.jsx
+++ react01/src/assets/pages/footer/footer.jsx
@@ -1,15 +1,24 @@
+import "./footer.css"
+
 const Footer = () => {
   return (
-    <>
-      <footer>
-        <p>
-          <a>Sergio Torres</a>
-        </p>
-        <ul>
-          <a href="github.com/gnunezoliv">GitHub</a>
-        </ul>
-      </footer>
-    </>
+    <footer className="footer">
+      <div className="copyright">
+        <p>&copy; 2024 My Website. All rights reserved.</p>
+      </div>
+      <div className="social-links">
+        <a href="#" target="_blank" >
+          <i>LinkedIn</i>
+        </a>
+        <a href="#" target="_blank" >
+          <i>GitHub</i>
+        </a>
+        <a href="#" target="_blank" >
+          <i>Instagram</i>
+        </a>
+      </div>
+      
+    </footer>
   );
 };
 export { Footer };
