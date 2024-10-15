e35cabd80ed6c44f386f8f4de467e4269945855b
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-15 10:57:51 -0600

    style: estilos inline en ul

--- react01/src/assets/pages/header/navbar.jsx
+++ react01/src/assets/pages/header/navbar.jsx
@@ -1,6 +1,27 @@
 import './navbar.css'
 
+const estiloArial = {
+  fontFamily:"Arial", 
+  color:"yellow"
+}
+
+const estiloCourier = {
+  fontFamily:"Courier New, monospace", 
+  color:"cyan"
+}
+
+
 const Navbar = () => {
+
+  const isArial = false;
+  let estiloAAplicar;
+
+  if( isArial ){
+    estiloAAplicar = estiloArial;
+  } else {
+    estiloAAplicar = estiloCourier;
+  }
+
   return (
     <>
       <nav className="navbar" >
@@ -9,7 +30,7 @@ const Navbar = () => {
           <img src="/img/ninja-128px.png" alt="Logo Ninja" />
         </div>
         {/* Opciones del menú */}
-        <ul className="nav-links">
+        <ul className="nav-links" style={ estiloAAplicar  } >
           <li>
             <a>Home</a>
           </li>
