bb91c9fb45e3a215e78c087eefc96f5a1c749f62
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-16 10:51:13 -0600

    feat: desestructuracion

--- react01/src/assets/components/card/card.jsx
+++ react01/src/assets/components/card/card.jsx
@@ -1,13 +1,31 @@
+
 /*
-properties = {
-  title:"Shampoo",
-  stock:50
-}
-  */
+ Un componente en React se renderiza baja ciertas condiciones:
+
+  - Cambio en las props: Si las props que recibe el componente cambian, este
+   se vuelve a renderizar.
+  - Cambio de estado( useState): Si el estado del componente cambia, este
+   se vuelve a renderizar.
+  - Cambio en el componente Parent: Si un parent se renderiza, los child también
+   lo harán.
+
+   Sintaxis de useState:
+   const [estado, setEstado] = useState(valorInicial);
+    - estado: Es el valor actual del estado.
+    - setEstado: Es la función que se usa para actualizar el valor del estado.
+    - valorInicial: Es el valor inicial que se le asigna al estado.
+
+   Reglas de uso de useState:
+    - useState solo se puede usar dentro de componentes funcionales.
+    - No se puede usar condicionalmente o en loops.
+    - El valor del estado se mantiene entre renderizados del componente
+*/
+
+import { useState } from "react";
 
 export const Card = ( props ) => {
 
-  let counter = 1;
+  const [counter, setCounter] = useState( 1 );
 
   const incrementHandle = ()=>{
     console.log("Estoy dentro de la función incremento")
---
+++ react01/src/assets/components/card/desestucturacion.js
@@ -0,0 +1,68 @@
+
+
+/*
+  La desestructuración en JavaScript es una característica 
+  que permite extraer valores de arreglos u objetos y asignarlos 
+  a variables de manera más directa y legible. Es muy útil para 
+  trabajar con datos complejos como objetos anidados o arrays largos.
+
+*/
+
+let a = 10;
+let b = 5;
+
+// Cambiar el valor de a->b y el valor de b->a
+let temporal = a; // 10
+a = b; // a: 5, b: 5
+b = temporal; // b: 10, a: 5
+
+// Aplicando la desestructuración
+let valueA = 10; 
+let valueB = 5;
+[ valueA, valueB] = [ valueB, valueA ];
+
+// Teniendo el sig. Array ["Steve", "Tessa", "Copito", "Tibio"]
+// Asignar el valor a Steve a la variable ivanna
+// Asignar el valor de Tessa a la variable serch
+
+const [ ivanna, serch, , eli ] = ["Steve", "Tessa", "Copito", "Tibio"];
+console.log( ivanna, serch, eli );
+console.log( {ivanna, serch, eli} );
+
+// Aplicando al desestructuración a objetos
+const datosSteve = {
+    name: "Steve",
+    age: 7,
+    color: [ "amarillo", "naranja" ],
+    owner: "Ivanna",
+    type: {
+        commonName: "Gecko Leopardo",
+        habitat: "Desierto"
+    }
+}
+
+// Imprimir el nombre y edad de Steve
+/*
+console.log( datosSteve.name, datosSteve.age );
+console.log( `
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${ datosSteve.name }</h5>
+    <p class="card-text">${ datosSteve.age }</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    ` );
+*/
+const {age, name: nombre, type: {commonName : nombreComun } } = datosSteve;
+console.log( `
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${ nombre }</h5>
+    <p class="card-text">${nombreComun} ${ age }</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    ` );
