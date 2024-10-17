3185c3725e23321741920f15f12362d7eaabdbd6
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-17 11:08:37 -0600

    style: margin en el container

--- react01/src/assets/pages/cards/cards.jsx
+++ react01/src/assets/pages/cards/cards.jsx
@@ -5,18 +5,18 @@ import Grid from "@mui/material/Grid2";
 export const Cards = () => {
   return (
     <>
-      <Container maxWidth="md">
+      <Container maxWidth="md" sx={ {my:3} }>
         <Grid container>
-          <Grid size={6}>
+          <Grid size={ {xs:6 , md:4} }>
             <Card title="Shampoo" stock={50} />
           </Grid>
-          <Grid size={6}>
+          <Grid size={ {xs:6 , md:4} }>
             <Card title="Desodorante" stock={10} />
           </Grid>
-          <Grid size={6}>
+          <Grid size={ {xs:6 , md:4} }>
             <Card title="Jabón corporal" stock={5} />
           </Grid>
-          <Grid size={6}>
+          <Grid size={ {xs:6 , md:4} }>
             <Card title="Perfume" stock={4} />
           </Grid>
         </Grid>
