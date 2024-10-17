1bde4c5cf10860fc9ec672bbe6a8b93bae68d3da
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-17 13:45:59 -0600

    feat: prop key en <li>

--- react01/src/assets/pages/about/about.jsx
+++ react01/src/assets/pages/about/about.jsx
@@ -1,6 +1,39 @@
+import { Container, Typography } from "@mui/material"
+
+
+/*
+ Renderización de valores especiales:
+  Si intentas renderizar los siguientes valores,
+  no aparecerán en la interfaz. Son ignorados por React.
+
+   - true
+   - false
+   - null
+   - undefined
+
+  Uso de list y keys
+  Cuando renderizas listas de elementos en React, cada elemento
+  debe tener un identificador único mediante la propiedad key.
+  Esto ayuda a React a optimizar el proceso de actualización del DOM.
+
+*/
+const dinamitaBuenaOnda = [ 
+  {name:"Sergio", numEmployee: 589},
+  {name:"Tan", numEmployee: 789},
+  {name:"Nan", numEmployee: 97},
+  {name:"Blanca", numEmployee: 23},
+];
 
 export const About = () => {
   return (
-    <div>About</div>
+    <Container>
+        <Typography variant="h2">About</Typography>
+        {true}{false}{null}{undefined}
+        <ul>
+          { dinamitaBuenaOnda.map( (person)=> <li key={person.numEmployee} >{person.name}</li> ) }
+        </ul>
+
+
+    </Container>
   )
 }
